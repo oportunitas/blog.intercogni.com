273b5ba8bfd872da4e0095f5e4a6fc7313064ec5
oportunitas <taibizzat165@gmail.com>
2024-08-28 17:32:28 +0700

    test: testing github oauth functionality

--- config/services.php
+++ config/services.php
@@ -14,6 +14,18 @@ return [
     |
     */
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://localhost:8000/auth/github/callback',
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
-    return view('welcome');
+    dump('calling welcome');
+    return view(view: 'welcome');
+});
+
+Route::get('/auth/github', function () {
+    dump('calling auth');
+    return Socialite::driver('github')->redirect();
+})->name('github.login');
+
+// Route::get('/auth/github/callback', function () {
+//     $user = Socialite::driver('github')->user();
+ 
+//     // $user->token
+// });
+
+Route::get('/auth/github/callback', function() {
+    $github_user =  Socialite::driver('github')->user();
+
+    $user = User::updateOrCreate([
+        'github_id' => $github_user->id,
+    ], [
+        'name' => $github_user->name,
+        'email' => $github_user->email,
+        'github_token' => $github_user->token,
+        'github_refresh_token' => $github_user->refreshToken
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/');
 });
