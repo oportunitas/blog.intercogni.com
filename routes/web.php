50d84905ea06f196da049655d2c1df079ea85541
oportunitas <taibizzat165@gmail.com>
2024-08-28 17:59:21 +0700

    feat(github oauth): implement github login functionality

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,19 @@ Route::get('/auth/github', function () {
     return Socialite::driver('github')->redirect();
 })->name('github.login');
 
-Route::get('/auth/github/callback', function () {
-    $user = Socialite::driver('github')->stateless()->user();
- 
-    // $user->token
+Route::get('/auth/github/callback', function() {
+    $githubUser = Socialite::driver('github')->stateless()->user();
+
+    $user = User::updateOrCreate([
+        'github_id' => $githubUser->id,
+    ], [
+        'name' => $githubUser->name,
+        'email' => $githubUser->email,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/');
 });
-
-// Route::get('/auth/github/callback', function() {
-//     $githubUser =  Socialite::driver('github')->user();
-
-//     $user = User::updateOrCreate([
-//         'github_id' => $githubUser->id,
-//     ], [
-//         'name' => $githubUser->name,
-//         'email' => $githubUser->email,
-//         'github_token' => $githubUser->token,
-//         'github_refresh_token' => $githubUser->refreshToken
-//     ]);
-
-//     Auth::login($user);
-
-//     return redirect('/');
-// });
